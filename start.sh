79c6cbbeb2883c9ada6f197ae59226751afd535e
Sharkgrammer <danielkeanekelly@gmail.com>
2020-02-13 14:52:28 +0000

    Script changes

--- start.sh
+++ start.sh
@@ -1,19 +1,19 @@
 #! /bin/sh
 echo "Starting Sonar"
 
-jsvc_location = "/usr/bin/jsvc"
-java_home = "/usr/lib/jvm/default-java"
-jar_paths = "Sonar-Secure-Server.jar:lib/commons-daemon-1.2.2.jar"
+jsvc_location="/usr/bin/jsvc"
+java_home="/usr/lib/jvm/default-java"
+jar_paths="Sonar-Secure-Server.jar:lib/commons-daemon-1.2.2.jar"
 
-name = "SonarDaemon"
-desc = "Sonar Daemon Service"
-class = "server.serverDaemon"
+name="SonarDaemon"
+desc="Sonar Daemon Service"
+class="server.serverDaemon"
 
-args = "start"
-user = "root"
-pid = "/var/run/$name.pid"
-out = "log/$name.out"
-err = "err/$name.err"
+args="start"
+user="root"
+pid="/var/run/$name.pid"
+out="log/$name.out"
+err="err/$name.err"
 
 jsvc_exec()
 {
@@ -26,3 +26,36 @@ echo "Starting the $desc..."
 
 # Start the service
 jsvc_exec
+
+echo "The $desc has started."
+;;
+stop)
+echo "Stopping the $desc..."
+
+# Stop the service
+jsvc_exec "-stop"
+
+echo "The $desc has stopped."
+;;
+restart)
+if [ -f "$pid" ]; then
+
+echo "Restarting the $desc..."
+
+# Stop the service
+jsvc_exec "-stop"
+
+# Start the service
+jsvc_exec
+
+echo "The $desc has restarted."
+else
+echo "Daemon not running, no action taken"
+exit 1
+fi
+;;
+*)
+echo "Usage: /etc/init.d/$name {start|stop|restart}" >&2
+exit 3
+;;
+esac
