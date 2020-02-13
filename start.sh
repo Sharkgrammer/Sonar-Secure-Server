f0684031edae5fd50f94cb82d3444a1f3372448a
Sharkgrammer <danielkeanekelly@gmail.com>
2020-02-13 15:28:14 +0000

    Script changes

--- start.sh
+++ start.sh
@@ -3,7 +3,8 @@ echo "Starting Sonar"
 
 jsvc_location="/usr/bin/jsvc"
 java_home="/usr/lib/jvm/default-java"
-jar_paths="Sonar-Secure-Server.jar:lib/commons-daemon-1.2.2.jar"
+jar_paths="$current_path/Sonar-Secure-Server.jar:$current_path/lib/commons-daemon-1.2.2.jar"
+current_path=$(pwd)
 
 name="SonarDaemon"
 desc="Sonar Daemon Service"
@@ -12,8 +13,8 @@ class="server.serverDaemon"
 args="start"
 user="root"
 pid="/var/run/$name.pid"
-out="log/$name.out"
-err="log/$name.err"
+out="$current_path/log/$name.out"
+err="$current_path/log/$name.err"
 
 script="$jsvc_location -home $java_home -cp $jar_paths -user $user -outfile $out -errfile $err -pidfile $pid $1 $class $args"
 
