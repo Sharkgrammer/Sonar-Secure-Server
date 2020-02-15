c1bef5d70d9952c233519a7ad7aff03a7b194432
Sharkgrammer <danielkeanekelly@gmail.com>
2020-02-15 15:49:57 +0000

    Script changes and jar change

--- start.sh
+++ start.sh
@@ -2,16 +2,17 @@
 echo "Starting Sonar"
 
 current_path=$(pwd)
+current_IP=$(curl ifconfig.me)
 jsvc_location="/usr/bin/jsvc"
 java_home="/usr/lib/jvm/default-java"
 jar_paths="$current_path/Sonar-Secure-Server.jar:$current_path/libs/commons-daemon-1.2.2.jar:$current_path/libs/log4j-1.2.17.jar:$current_path/libs/shark-protocol.jar"
 
 name="SonarDaemon"
-desc="Sonar Daemon Service"
+desc="Sonar Daemon"
 class="server.serverDaemon"
 
 args="start"
-user="root"
+user=$(whoami)
 pid="/var/run/$name.pid"
 out="$current_path/log/$name.out"
 err="$current_path/log/$name.err"
@@ -22,7 +23,7 @@ echo $script
 
 jsvc_exec()
 {
-sudo $script
+$script
 }
 
 echo "Starting the $desc..."
@@ -30,4 +31,4 @@ echo "Starting the $desc..."
 # Start the service
 jsvc_exec
 
-echo "The $desc has started, apparently"
+echo "The $desc has started, running on $current_IP:6000"
