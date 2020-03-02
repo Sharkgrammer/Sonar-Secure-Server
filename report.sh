a1c1c13b7bbfc07a1f15cf37b280d0c79ec4a0ed
Sharkgrammer <danielkeanekelly@gmail.com>
2020-03-02 17:12:56 +0000

    Reporting function

---
+++ report.sh
@@ -0,0 +1,40 @@
+#! /bin/sh
+
+dataTime=0
+dataAmt=0
+decryptionTime=0
+decryptionAmt=0
+
+dedel="md:"
+dadel="dp:"
+
+while read line; do
+	if [[ "${str}" == *dadel* ]] ;then
+		tempTime=${line%"$dadel"}
+	
+		if [$tempTime != 0]; then
+			$dataAmt+=1
+			$dataTime+=$tempTime
+		fi
+		$dataTime+=1
+	fi
+	 
+	if [[ "${str}" == *dedel* ]] ;then
+		tempTime=${line%"$dedel"}
+	
+		if [$tempTime != 0]; then
+			$decryptionAmt+=1
+			$decryptionTime+=$tempTime
+		fi
+			
+	fi
+done < "log/SonarDaemon.out"
+
+startStr="Data takes an average of "
+
+resultDec=$decryptionTime/$decryptionAmt
+resultData=$dataTime/$dataAmt
+
+
+echo "Data takes an average of " + $resultData + " milliseconds to process"
+echo "Data takes an average of " + $resultDec + " milliseconds to decrypt"
